c32ea87c3fb2d580a04d73d87a43279c313748e0
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2017-04-28 12:54:47 -0600

    Primer prototipo funcional

--- src/hoja9/Main.java
+++ src/hoja9/Main.java
@@ -1,5 +1,15 @@
 package hoja9;
 
-public class Main {
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Scanner teclado= new Scanner(System.in);
+		Texto trad= new Texto();
+		trad.s();
+		trad.lectura();
+		System.out.println(trad.traduccion(trad.lectura()));
+	}
 }
--- src/hoja9/Texto.java
+++ src/hoja9/Texto.java
@@ -4,16 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
+import javax.swing.JFileChooser;
+
 import com.sun.nio.sctp.Association;
 
 public class Texto {
-	
-	TwoThreeTree<String,String> b = new TwoThreeTree();
-	
-	
+	FactoryTree facTree = new FactoryTree();
+	TwoThreeTree<String,String> b = new TwoThreeTree<String, String>();
 	public void s() throws IOException {
 		String a = "";
-		BufferedReader txt = new BufferedReader(new FileReader("freedic-eng-spa.txt"));
+		BufferedReader txt = new BufferedReader(new FileReader("freedict-eng-spa.dic"));
 		String palabra;
 		while((palabra=txt.readLine())!=null){
 			a=a+" "+palabra;
@@ -24,8 +25,45 @@ public class Texto {
 	public void llenar(String palabra){
 		String cadena = palabra.replaceAll(" ", ",");
 		cadena = cadena.replaceAll("\t", ",");
-		String palabras[]= palabra.split(",");
+		String palabras[]= cadena.split(",");
 		b.put(palabras[0], palabras[1]);
+	}
+	public String lectura() throws IOException{
+		String a = ""; 
+	
+		JFileChooser archivo = new JFileChooser();
+		archivo.showOpenDialog(null);
+		BufferedReader reader = new BufferedReader(new FileReader(archivo.getSelectedFile().getPath()));
+		String pal;
+		while(reader.ready()){
+			while ((pal=reader.readLine())!= null){
+				a=a+" "+pal;
+			}
+			//String w=tree.find(new Association<String, String>("yes","si")).toString();
+			return a;
+		}
+		return a;
+	}
+	
+	/**
+	 * @param texto a ser traducido
+	 * @return texto traducid0
+	 * Metodo toma un texto, lo separa, y luego busca coincidencias en el binary tree. De no encontrarlas agrega a la cadena de "texto traducido" la palabra
+	 * ingresada entre asteriscos
+	 */
+	public String traduccion(String texto){
+		String traducido="";
+		texto = texto.toLowerCase();
+		String palabrasATraducir[] = texto.split("\\s+");
+		for (int i=0; i<palabrasATraducir.length;i++){
+			String x =palabrasATraducir[i];
+			if ( b.get(palabrasATraducir[i]) != null){
+				traducido = traducido + b.get(palabrasATraducir[i]).toString() +" ";
+			}else{
+				traducido = traducido + "*"+x+"* ";
+			}
+		}
+		return traducido;
 	}
 
 }

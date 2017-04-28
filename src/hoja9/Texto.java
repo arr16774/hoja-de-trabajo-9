6a42c56782cc9e4ae2d586a1b55deef2f0f56556
arr16774 <caarroyavepadilla@gmail.com>
2017-04-28 12:35:09 -0600

    askd
    asidpk

--- src/hoja9/Texto.java
+++ src/hoja9/Texto.java
@@ -1,5 +1,31 @@
 package hoja9;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import com.sun.nio.sctp.Association;
+
 public class Texto {
+	
+	TwoThreeTree<String,String> b = new TwoThreeTree();
+	
+	
+	public void s() throws IOException {
+		String a = "";
+		BufferedReader txt = new BufferedReader(new FileReader("freedic-eng-spa.txt"));
+		String palabra;
+		while((palabra=txt.readLine())!=null){
+			a=a+" "+palabra;
+			llenar(palabra);
+			}
+		}
+	
+	public void llenar(String palabra){
+		String cadena = palabra.replaceAll(" ", ",");
+		cadena = cadena.replaceAll("\t", ",");
+		String palabras[]= palabra.split(",");
+		b.put(palabras[0], palabras[1]);
+	}
 
 }
